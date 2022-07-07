a1dfdf01a937df35fc8a59655bc6415430f5ff82
kYpranite <39254153+kYpranite@users.noreply.github.com>
2022-07-06 23:33:27 -0700

    server file

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000);
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + "/index.html");
 })
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, ()=>{
+    console.log('listening on *:' + port);
+});
